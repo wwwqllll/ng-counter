876cf94eb25eb5f1f3507a20ec9aa1b9d774fabb
Qinlan Weng <QWeng@slb.com>
2022-11-04 14:10:29 +0800

    feat: add app counter

---
+++ src/app/counter/counter.component.spec.ts
@@ -0,0 +1,52 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CounterComponent } from './counter.component';
+
+describe('CounterComponent', () => {
+  let component: CounterComponent;
+  let fixture: ComponentFixture<CounterComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CounterComponent ]
+    })
+      .compileComponents();
+
+    fixture = TestBed.createComponent(CounterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should add 1 when click plus button', () => {
+    // given
+    component.count = 0;
+    const plusBtn = fixture.nativeElement.querySelector('[data-test="plusBtn"]');
+    
+    // when
+    plusBtn.click();
+    fixture.detectChanges();
+
+    // then
+    const displayCount = fixture.nativeElement.querySelector('[data-test="displayCount"]');
+    expect(displayCount.textContent).toEqual('number: 1');
+  });
+
+  it('should minus 1 when click minus button', () => {
+    // given
+    component.count = 0;
+    const minusBtn = fixture.nativeElement.querySelector('[data-test="minusBtn"]');
+    
+    // when
+    minusBtn.click();
+    fixture.detectChanges();
+
+    // then
+    const displayCount = fixture.nativeElement.querySelector('[data-test="displayCount"]');
+    expect(displayCount.textContent).toEqual('number: -1');
+  });
+  
+});
---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.scss']
+})
+export class CounterComponent implements OnInit {
+
+  count: number = 0;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  inscrease(): void{
+    this.count = this.count + 1;
+  }
+
+  decrease(): void{
+    this.count = this.count - 1;
+  }
+
+}
